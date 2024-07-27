a85461805fbfc86ef4861f8662d76f36e0b000f5
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-27 22:13:29 +0100

    Write 'toggle-film' and 'all' routes

--- server/app/routes/protected.ts
+++ server/app/routes/protected.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 
-import { verifyToken } from '../middleware/verifyToken.js';
 import { expressEnv } from '../../utils/environmentChecks.js';
+import { verifyToken } from '../middleware/verifyToken.js';
+import { syncUserFilms } from '../services/filmService.js';
+import { updateUserFilm, getUserFilms } from '../services/userService';
 
 interface CustomRequest extends Request {
 	userId?: string;
@@ -13,6 +15,54 @@ protectedRouter.use(verifyToken);
 protectedRouter.get('/protected', (req: CustomRequest, res: Response) => {
 	res.json({ message: 'This is a protected route', userId: req.userId });
 });
+
+protectedRouter.get(
+	'/all',
+	verifyToken,
+	async (req: CustomRequest, res: Response) => {
+		try {
+			const userId = req.userId;
+
+			if (!userId) {
+				return res.status(400).json({ error: 'User ID is required' });
+			}
+
+			const userFilms = await getUserFilms(userId);
+			res.json(userFilms);
+		} catch (error) {
+			console.error('Error fetching user films:', error);
+			res.status(500).json({ error: 'Internal server error' });
+		}
+	}
+);
+
+protectedRouter.post(
+	'/toggle-film',
+	verifyToken,
+	async (req: CustomRequest, res: Response) => {
+		try {
+			const userId = req.userId;
+			const { filmId, seen } = req.body;
+
+			if (!userId) {
+				return res.status(401).json({ error: 'User not authenticated' });
+			}
+
+			if (typeof filmId !== 'string' || typeof seen !== 'boolean') {
+				return res.status(400).json({ error: 'Invalid request body' });
+			}
+
+			await syncUserFilms(userId, filmId, seen);
+
+			res.status(200).json({
+				message: 'Film seen status updated successfully',
+			});
+		} catch (error) {
+			console.error('Error toggling film seen status:', error);
+			res.status(500).json({ error: 'Internal server error' });
+		}
+	}
+);
 
 protectedRouter.post('/sign-out', (req: Request, res: Response) => {
 	res.clearCookie('token', {
