3a828f531cdb55f78576d734581b45a9cda6d5f9
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-27 17:15:09 +0100

    Test public routes (All passing)

--- test/server/app.test.ts
+++ test/server/app.test.ts
@@ -1,148 +1,159 @@
 import { describe, it, beforeAll, beforeEach, expect } from 'vitest';
-// import { createServer } from 'http';
-// import { AddressInfo } from 'net';
 import supertest from 'supertest';
 import app from '../../server/app/app.js';
 import jwt from 'jsonwebtoken';
 
 const request = supertest(app);
 
-describe('Welcome message', () => {
-  it('should return 200 on /api', async () => {
-    const response = await request.get('/api');
-    expect(response.status).toBe(200);
-  });
+import { jwtSecret } from '../../server/utils/environmentChecks.js';
 
-  it('should return JSON with "MetaCritic100 API"', async () => {
-    const response = await request
-      .get('/api')
-      .expect('Content-Type', /json/)
-      .expect(200);
+describe('API welcome message', () => {
+	it('should return 200 on /api', async () => {
+		const response = await request.get('/api');
+		expect(response.status).toBe(200);
+	});
 
-    expect(response.body).toHaveProperty('message', 'MetaCritic100 API');
-  });
+	it('should return JSON with "MetaCritic100 API"', async () => {
+		const response = await request
+			.get('/api')
+			.expect('Content-Type', /json/)
+			.expect(200);
+
+		expect(response.body).toHaveProperty('message', 'MetaCritic100 API');
+	});
 });
 
-describe('API Tests', () => {
-  const jwtSecret = process.env.EXPRESS_JWT_SECRET;
-  let validToken: string;
+const validToken = jwt.sign(
+	{
+		email: 'dan@example.com',
+		password: 'securePassword',
+		userId: 1,
+	},
+	jwtSecret,
+	{ expiresIn: '1h' }
+);
 
-  beforeAll(() => {
-    validToken = jwt.sign(
-      {
-        email: 'wronguser@gmail.com',
-        password: 'securePassword',
-        userId: 1,
-      },
-      jwtSecret!,
-      {
-        expiresIn: '1h',
-      }
-    );
-  });
+describe('POST /api/sign-in', () => {
+	it('should fail login with incorrect email', async () => {
+		const response = await request
+			.post('/api/sign-in')
+			.send({ email: 'wronguser@gmail.com', password: 'securePassword' })
+			.expect('Content-Type', /json/)
+			.expect(401);
 
-  describe('POST /api/sign-in', () => {
-    it('should login successfully with correct credentials', async () => {
-      const response = await request
-        .post('/api/sign-in')
-        .send({ email: 'user@gmail.com', password: 'securePassword' })
-        .expect('Content-Type', /json/)
-        .expect(200);
+		expect(response.body).toHaveProperty('message', 'Invalid credentials');
+		expect(response.headers['set-cookie']).toBeUndefined();
+	});
 
-      expect(response.body).toHaveProperty('message', 'Login successful');
-      expect(response.headers['set-cookie']).toBeDefined();
-    });
+	it('should fail login with incorrect password', async () => {
+		const response = await request
+			.post('/api/sign-in')
+			.send({ email: 'user@gmail.com', password: 'wrongPassword' })
+			.expect('Content-Type', /json/)
+			.expect(401);
 
-    it('should fail login with incorrect email', async () => {
-      const response = await request
-        .post('/api/sign-in')
-        .send({ email: 'wronguser@gmail.com', password: 'securePassword' })
-        .expect('Content-Type', /json/)
-        .expect(401);
+		expect(response.body).toHaveProperty('message', 'Invalid credentials');
+		expect(response.headers['set-cookie']).toBeUndefined();
+	});
 
-      expect(response.body).toHaveProperty('message', 'Invalid credentials');
-      expect(response.headers['set-cookie']).toBeUndefined();
-    });
+	it('should login successfully with correct credentials', async () => {
+		const response = await request
+			.post('/api/sign-in')
+			.send({ email: 'dan@gmail.com', password: 'securePassword' })
+			.expect('Content-Type', /json/)
+			.expect(200);
 
-    it('should fail login with incorrect password', async () => {
-      const response = await request
-        .post('/api/sign-in')
-        .send({ email: 'user@gmail.com', password: 'wrongPassword' })
-        .expect('Content-Type', /json/)
-        .expect(401);
+		expect(response.body).toHaveProperty('message', 'Login successful');
+		expect(response.headers['set-cookie']).toBeDefined();
+	});
+});
 
-      expect(response.body).toHaveProperty('message', 'Invalid credentials');
-      expect(response.headers['set-cookie']).toBeUndefined();
-    });
-  });
+describe('GET /api/validate-token', () => {
+	it('should return 200 if a valid token is provided', async () => {
+		const response = await request
+			.get('/api/validate-token')
+			.set('Cookie', `token=${validToken}`);
 
-  describe('Protected Routes', () => {
-    beforeEach(async () => {
-      await request.get('/clear-cookies');
-    });
+		expect(response.status).toBe(200);
+		expect(response.body).toEqual({
+			userId: 1,
+		});
+	});
 
-    it('should deny access without token', async () => {
-      const response = await request.get('/api/protected');
-      expect(response.status).toBe(401);
-      expect(response.body).toHaveProperty('message', 'No token provided');
-    });
+	it(`should return 'No token provided' if no token is provided`, async () => {
+		const response = await request.get('/api/validate-token');
 
-    it('should deny access with invalid token', async () => {
-      const response = await request
-        .get('/api/protected')
-        .set('Cookie', 'token=invalidToken');
+		expect(response.status).toBe(401);
+		expect(response.body).toEqual({
+			message: 'No token provided',
+		});
+	});
 
-      expect(response.status).toBe(401);
-      expect(response.body).toHaveProperty('message', 'Invalid token');
-    });
+	const incorrectJwtSecret = 'abcdefg123456789';
 
-    it('should access protected route with valid token', async () => {
-      const response = await request
-        .get('/api/protected')
-        .set('Cookie', `token=${validToken}`);
+	const invalidToken = jwt.sign(
+		{
+			email: 'dan@example.com',
+			password: 'securePassword',
+			userId: 1,
+		},
+		incorrectJwtSecret,
+		{ expiresIn: '1h' }
+	);
 
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('message', 'This is a protected route');
-      expect(response.body).toHaveProperty('userId', 1);
-    });
-  });
+	it('should return 401 if an invalid token is provided', async () => {
+		const response = await request
+			.get('/api/validate-token')
+			.set('Cookie', `token=${invalidToken}`);
 
-  describe('POST /api/sign-out', () => {
-    it('should logout successfully and clear the token cookie', async () => {
-      const response = await request
-        .post('/api/sign-out')
-        .set('Cookie', `token=${validToken}`);
+		expect(response.status).toBe(401);
+		expect(response.body).toEqual({
+			message: 'Invalid token',
+		});
+	});
+});
 
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('message', 'Logged out successfully');
+describe('POST /api/sign-out', () => {
+	it('should logout successfully and clear the token cookie', async () => {
+		const response = await request
+			.post('/api/sign-out')
+			.set('Cookie', `token=${validToken}`);
 
-      const cookies = response.headers['set-cookie'];
-      expect(cookies).toBeDefined();
+		expect(response.status).toBe(200);
+		expect(response.body).toHaveProperty(
+			'message',
+			'Logged out successfully'
+		);
 
-      let tokenCookie: string | undefined;
+		const cookies = response.headers['set-cookie'];
+		expect(cookies).toBeDefined();
 
-      if (Array.isArray(cookies)) {
-        tokenCookie = cookies.find((cookie) => cookie.startsWith('token='));
-      } else if (typeof cookies === 'string') {
-        tokenCookie = cookies.startsWith('token=') ? cookies : undefined;
-      }
+		let tokenCookie: string | undefined;
 
-      expect(tokenCookie).toBeDefined();
-      expect(tokenCookie).toContain('token=;');
-      expect(tokenCookie).toContain('HttpOnly');
+		if (Array.isArray(cookies)) {
+			tokenCookie = cookies.find((cookie) => cookie.startsWith('token='));
+		} else if (typeof cookies === 'string') {
+			tokenCookie = cookies.startsWith('token=') ? cookies : undefined;
+		}
 
-      if (process.env.VITE_REACT_ENV === 'production') {
-        expect(tokenCookie).toContain('Secure');
-        expect(tokenCookie).toContain('SameSite=Strict');
-      } else {
-        expect(tokenCookie).toContain('SameSite=None');
-      }
-    });
+		expect(tokenCookie).toBeDefined();
+		expect(tokenCookie).toContain('token=;');
+		expect(tokenCookie).toContain('HttpOnly');
 
-    it('should return 200 even if not logged in', async () => {
-      const response = await request.post('/api/sign-out');
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('message', 'Logged out successfully');
-    });
-  });
+		if (process.env.VITE_REACT_ENV === 'production') {
+			expect(tokenCookie).toContain('Secure');
+			expect(tokenCookie).toContain('SameSite=Strict');
+		} else {
+			expect(tokenCookie).toContain('SameSite=None');
+		}
+	});
+
+	it('should return 200 even if not logged in', async () => {
+		const response = await request.post('/api/sign-out');
+		expect(response.status).toBe(200);
+		expect(response.body).toHaveProperty(
+			'message',
+			'Logged out successfully'
+		);
+	});
 });
