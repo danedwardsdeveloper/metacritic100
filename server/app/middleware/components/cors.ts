3471309f49c6e09a36e7d807278d4f95516d1e04
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-27 22:14:09 +0100

    Add new methods

--- server/app/middleware/components/cors.ts
+++ server/app/middleware/components/cors.ts
@@ -1,17 +1,16 @@
-import { Express } from 'express'
-import cors from 'cors'
+import { Express } from 'express';
+import cors from 'cors';
 
-import { allowedOrigins } from '../../../utils/environmentChecks.js'
+import { allowedOrigins } from '../../../utils/environmentChecks.js';
 
 export default function setupCors(app: Express) {
 	app.use(
 		cors({
 			origin: allowedOrigins,
-			methods: ['GET', 'POST'],
+			methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
 			allowedHeaders: ['Content-Type', 'Authorization'],
 			credentials: true,
-			optionsSuccessStatus: 200
+			optionsSuccessStatus: 200,
 		})
-	)
+	);
 }
-
