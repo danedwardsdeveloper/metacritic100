22de0b9c9a1663827e3e108f1690c535c71108dd
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-27 19:03:14 +0100

    Apply verifyToken to all routes

--- server/app/routes/protected.ts
+++ server/app/routes/protected.ts
@@ -1,15 +1,16 @@
 import express, { Request, Response } from 'express';
+
 import { verifyToken } from '../middleware/verifyToken.js';
-import dotenv from 'dotenv';
-dotenv.config();
+import { expressEnv } from '../../utils/environmentChecks.js';
 
 interface CustomRequest extends Request {
 	userId?: string;
 }
 
 const protectedRouter = express.Router();
+protectedRouter.use(verifyToken);
 
-protectedRouter.get('/protected', verifyToken, (req: CustomRequest, res: Response) => {
+protectedRouter.get('/protected', (req: CustomRequest, res: Response) => {
 	res.json({ message: 'This is a protected route', userId: req.userId });
 });
 
@@ -17,9 +18,9 @@ protectedRouter.post('/sign-out', (req: Request, res: Response) => {
 	res.clearCookie('token', {
 		httpOnly: true,
 		secure: true,
-		sameSite: process.env.VITE_NODE_ENV === 'production' ? 'strict' : 'none',
+		sameSite: expressEnv === 'production' ? 'strict' : 'none',
 	});
-	res.status(200).json({ message: 'Logged out successfully' });
+	res.status(200).json({ message: 'Signed out successfully' });
 });
 
 export default protectedRouter;
