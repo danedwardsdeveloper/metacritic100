463c0a218f84bd027979eada1dd103c939232ee9
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-27 12:55:47 +0100

    Incorporate ErrorElement

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,6 +12,7 @@ import FilmsList from './app/components/FilmsList';
 import Protected from './app/components/Protected';
 import SignIn from './app/components/SignIn';
 import SignOut from './app/components/SignOut';
+import ErrorElement from './app/components/ErrorElement';
 
 import './index.tailwind.css';
 
@@ -20,11 +21,12 @@ import { FilmsProvider } from './app/contexts/FilmsContext';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
-		<Route path="/" element={<App />}>
+		<Route path="/" element={<App />} errorElement={<ErrorElement />}>
 			<Route index element={<FilmsList />} />
 			<Route path="protected" element={<Protected />} />
 			<Route path="sign-in" element={<SignIn />} />
 			<Route path="sign-out" element={<SignOut />} />
+			<Route path="*" element={<ErrorElement />} />
 		</Route>
 	)
 );
