29a9b24f1fce18053012be1c8cb33fee263e53db
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-28 10:39:09 +0100

    Clean User model

---
+++ server/app/models/User.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document } from 'mongoose';
+
+export interface IUserFilm {
+	filmId: string;
+	seen: boolean;
+	notes: string;
+}
+
+export interface IUser extends Document {
+	userId: string;
+	name: string;
+	email: string;
+	password: string;
+	films: IUserFilm[];
+}
+
+const UserSchema = new mongoose.Schema<IUser>({
+	userId: {
+		type: String,
+		default: () => new mongoose.Types.ObjectId().toString(),
+	},
+	name: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	films: [
+		{
+			filmId: String,
+			seen: Boolean,
+			notes: String,
+		},
+	],
+});
+
+export const User = mongoose.model<IUser>('User', UserSchema);
