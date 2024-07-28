ba952867fbcff7701a0e767acf749752176e11ff
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-28 10:40:53 +0100

    Move toggleFilm db query

--- server/app/services/filmService.ts
+++ server/app/services/filmService.ts
@@ -1,6 +1,25 @@
 import { Film, IFilm } from '../models/Film';
 import { User } from '../models/User';
 
+export async function toggleFilm(
+	userId: string,
+	filmId: string
+): Promise<void> {
+	const user = await User.findOne({ userId });
+	if (!user) {
+		throw new Error('User not found');
+	}
+
+	const filmIndex = user.films.findIndex((film) => film.filmId === filmId);
+	if (filmIndex === -1) {
+		throw new Error('Film not found for this user');
+	}
+
+	user.films[filmIndex].seen = !user.films[filmIndex].seen;
+
+	await user.save();
+}
+
 export async function addFilm(filmData: Partial<IFilm>): Promise<void> {
 	const film = new Film(filmData);
 	await film.save();
