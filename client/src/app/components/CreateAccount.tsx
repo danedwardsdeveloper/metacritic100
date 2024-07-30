609facc0b7e4910139aba545a5c98fb325d47a87
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-30 11:04:25 +0100

    Update logo alt text

--- client/src/app/components/CreateAccount.tsx
+++ client/src/app/components/CreateAccount.tsx
@@ -4,7 +4,7 @@ export default function CreateAccount() {
 			<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
 					<img
-						alt="Your Company"
+						alt="MetaCritic 100"
 						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
 						className="mx-auto h-10 w-auto"
 					/>
