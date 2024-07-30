46e3603e9902422b7166f3001286bb6532247005
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-30 10:57:44 +0100

    Create About.tsx

---
+++ client/src/app/components/About.tsx
@@ -0,0 +1,48 @@
+export default function About() {
+	return (
+		<>
+			<div className="px-6 py-24 sm:py-32 lg:px-8">
+				<div className="mx-auto max-w-2xl text-center">
+					<p className="text-base font-semibold leading-7 text-indigo-600">
+						The world's best films
+					</p>
+					<h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+						MetaCritic 100
+					</h2>
+					<p className="mt-6 text-lg leading-8 text-gray-600">
+						MetaCritic 100 is an engaging hobby project that allows film
+						enthusiasts to track their viewing progress through the
+						top-rated films on Metacritic. This fun, unofficial site is
+						not affiliated with Metacritic but instead uses their ratings
+						as a foundation for a personal movie-watching challenge. It
+						offers users a playful way to explore and track their journey
+						through critically acclaimed cinema, turning the pursuit of
+						watching top-rated films into an enjoyable, goal-oriented
+						experience.
+					</p>
+
+					<p className="mt-6 text-lg leading-8 text-gray-600">
+						Metacritic is a renowned review aggregator that compiles
+						critic reviews for various media, including films, TV shows,
+						music, and video games. For movies, Metacritic calculates a
+						weighted average of critic scores, resulting in a "Metascore"
+						ranging from 0 to 100. This score provides a quick snapshot of
+						critical consensus, with higher scores indicating more
+						favorable reviews.
+					</p>
+
+					<p className="mt-6 text-lg leading-8 text-gray-600">
+						MetaCritic 100 takes inspiration from this system, focusing
+						specifically on the highest-rated films. While not officially
+						connected to Metacritic, this project showcases a unique
+						intersection of technology and cinema appreciation. It
+						combines web development skills with a passion for film,
+						creating a platform where movie buffs can challenge themselves
+						to watch and keep track of some of the most critically
+						acclaimed films of all time.
+					</p>
+				</div>
+			</div>
+		</>
+	);
+}
