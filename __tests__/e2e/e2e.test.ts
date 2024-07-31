1e2da9a3c8be6b943065e66ea8aa19058df96fe9
Dan Edwards <daniel.edwards96@yahoo.com>
2024-07-31 14:30:54 +0100

    Fix tests

--- __tests__/e2e/e2e.test.ts
+++ __tests__/e2e/e2e.test.ts
@@ -1,34 +1,141 @@
 import { describe, it, expect, afterAll, beforeAll } from 'vitest';
 import puppeteer, { Browser, Page } from 'puppeteer';
 
-describe('Checkbox Count and Text Verification Test', () => {
+describe('Desktop main elements', () => {
 	let browser: Browser;
 	let page: Page;
-	let checkedCount: number = 0;
 
 	beforeAll(async () => {
 		browser = await puppeteer.launch({ headless: true });
 		page = await browser.newPage();
+		await page.setViewport({ width: 1204, height: 789 });
+		await page.goto('http://localhost:5173/');
 	});
 
-	it('should count 100 checkboxes and record the number of checked boxes', async () => {
-		await page.goto('http://localhost:5173/');
+	it('should have a link to "/" that says "MetaCritic 100"', async () => {
+		const link = await page.$('a[href="/"]');
+		expect(link).not.toBeNull();
+		const text = await page.evaluate((el) => el?.textContent, link);
+		expect(text).toBe('MetaCritic 100');
+	});
 
-		const checkboxes = await page.$$('input[type="checkbox"]');
-		const totalCheckboxes = checkboxes.length;
+	it('should have a link to "/about" that says "About"', async () => {
+		const link = await page.$('a[href="/about"]');
+		expect(link).not.toBeNull();
+		const text = await page.evaluate((el) => el?.textContent, link);
+		expect(text).toBe('About');
+	});
 
-		checkedCount = await page.evaluate(() => {
-			const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-			return Array.from(checkboxes).filter(
-				(checkbox): checkbox is HTMLInputElement =>
-					checkbox instanceof HTMLInputElement && checkbox.checked
-			).length;
+	it('should display a film counter in the menu bar', async () => {
+		await page.waitForSelector('nav');
+
+		const navContent = await page.evaluate(() => {
+			const nav = document.querySelector('nav');
+			return nav ? nav.textContent : null;
 		});
 
-		console.log(`Total number of checkboxes: ${totalCheckboxes}`);
-		console.log(`Number of checked checkboxes: ${checkedCount}`);
+		expect(navContent).not.toBeNull();
 
-		expect(totalCheckboxes).toBe(100);
+		if (navContent) {
+			const match = navContent.match(/(\d+)\s*\/\s*100/);
+
+			expect(match).not.toBeNull();
+			if (match) {
+				const number = parseInt(match[1]);
+				expect(number).toBeGreaterThanOrEqual(0);
+				expect(number).toBeLessThanOrEqual(100);
+			}
+		} else {
+			throw new Error('Nav content is null');
+		}
+	});
+
+	it('should have text elements that say "Dekalog", "Boyhood", and "The Godfather"', async () => {
+		const texts = ['Dekalog', 'Boyhood', 'The Godfather'];
+		for (const text of texts) {
+			const elements = await page.$$('h3');
+			for (const element of elements) {
+				const content = await element.evaluate((el) =>
+					el.textContent?.trim().toLowerCase()
+				);
+				if (content === text.toLowerCase()) {
+					break;
+				}
+			}
+		}
+	});
+
+	afterAll(async () => {
+		if (browser) {
+			await browser.close();
+		}
+	});
+});
+
+describe('Mobile main elements', () => {
+	let browser: Browser;
+	let page: Page;
+
+	beforeAll(async () => {
+		browser = await puppeteer.launch({ headless: true });
+		page = await browser.newPage();
+		await page.setViewport({ width: 375, height: 592 });
+		await page.goto('http://localhost:5173/');
+	});
+
+	it('should have a link to "/" that says "MetaCritic 100"', async () => {
+		const link = await page.$('a[href="/"]');
+		expect(link).not.toBeNull();
+		const text = await page.evaluate((el) => el?.textContent, link);
+		expect(text).toBe('MetaCritic 100');
+	});
+
+	it('should have a main menu button', async () => {
+		const button = await page.$('button:has(svg)');
+		expect(button).not.toBeNull();
+		const span = await button!.$('span.sr-only');
+		expect(span).not.toBeNull();
+		const text = await page.evaluate((el) => el?.textContent, span);
+		expect(text).toBe('Open main menu');
+	});
+
+	it('should display a film counter in the menu bar', async () => {
+		await page.waitForSelector('nav');
+
+		const navContent = await page.evaluate(() => {
+			const nav = document.querySelector('nav');
+			return nav ? nav.textContent : null;
+		});
+
+		expect(navContent).not.toBeNull();
+
+		if (navContent) {
+			const match = navContent.match(/(\d+)\s*\/\s*100/);
+
+			expect(match).not.toBeNull();
+			if (match) {
+				const number = parseInt(match[1]);
+				expect(number).toBeGreaterThanOrEqual(0);
+				expect(number).toBeLessThanOrEqual(100);
+			}
+		} else {
+			throw new Error('Nav content is null');
+		}
+	});
+
+	it('should have text elements that say "Dekalog", "Boyhood", and "The Godfather"', async () => {
+		const texts = ['Dekalog', 'Boyhood', 'The Godfather'];
+		for (const text of texts) {
+			const elements = await page.$$('h3');
+			for (const element of elements) {
+				const content = await element.evaluate((el) =>
+					el.textContent?.trim().toLowerCase()
+				);
+				if (content === text.toLowerCase()) {
+					break;
+				}
+			}
+		}
 	});
 
 	afterAll(async () => {
