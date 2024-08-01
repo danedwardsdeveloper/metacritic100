a6901ec0e66fcacfc3afad914cdbbbea3af2a77f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-01 17:05:06 +0100

    Add metadata types

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -43,3 +43,19 @@ export interface UserContextType {
 	toggleFilm: (filmId: string) => void;
 	resetUser: () => void;
 }
+
+export interface MetadataContent {
+	siteName: string;
+	defaultTitle: string;
+	defaultDescription: string;
+	author: string;
+	siteUrl: string;
+	defaultImage: string;
+}
+
+export interface MetadataProps {
+	title?: string;
+	description?: string;
+	pageName?: string;
+	slug?: string;
+}
