a11de9839bc3d8b0fb23aab489fab3a6d8e874aa
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-01 17:06:03 +0100

    Add metadata, fix form spacing issues

--- client/src/app/components/CreateAccount.tsx
+++ client/src/app/components/CreateAccount.tsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 
 import { useUser } from '../contexts/UserContext.tsx';
-import { createAccountService, Film } from '../services/UserService.ts';
+import { createAccountService } from '../services/UserService.ts';
+import { Film } from '../../types';
+import Metadata from './Metadata';
 
 export default function CreateAccount() {
 	const [name, setName] = useState('Ben');
@@ -48,6 +50,8 @@ export default function CreateAccount() {
 
 	return (
 		<>
+			<Metadata pageName="Create an account" slug="create-account" />
+
 			<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
 					<img
@@ -81,7 +85,9 @@ export default function CreateAccount() {
 									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
 								/>
 							</div>
+						</div>
 
+						<div>
 							<div className="flex items-center justify-between">
 								<label
 									htmlFor="email"
@@ -102,6 +108,9 @@ export default function CreateAccount() {
 									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 								/>
 							</div>
+						</div>
+
+						<div>
 							<div className="flex items-center justify-between">
 								<label
 									htmlFor="password"
@@ -109,14 +118,6 @@ export default function CreateAccount() {
 								>
 									Create a password
 								</label>
-								{/* <div className="text-sm">
-									<a
-										href="#"
-										className="font-semibold text-indigo-600 hover:text-indigo-500"
-									>
-										Forgot password?
-									</a>
-								</div> */}
 							</div>
 							<div className="mt-2">
 								<input
