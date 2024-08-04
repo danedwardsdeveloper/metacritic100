3e8c12295ec2c15bd4654e8f1fdd25e058ee48ad
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-04 08:59:36 +0100

    Create ThemeOptions.tsx

---
+++ client/src/app/components/ThemeOptions.tsx
@@ -0,0 +1,55 @@
+import { MenuItem } from '@headlessui/react';
+import {
+	SunIcon,
+	MoonIcon,
+	ComputerDesktopIcon,
+} from '@heroicons/react/24/outline';
+
+import { useTheme } from '../contexts/useTheme';
+import type { Theme } from '../../types';
+
+// import { cleanTailwindClassNames } from '../utils/utils';
+
+export default function ThemeOptions() {
+	const { theme, setTheme } = useTheme();
+
+	const options: { text: string; icon: React.ElementType; value: Theme }[] = [
+		{
+			text: 'Light',
+			icon: SunIcon,
+			value: 'light',
+		},
+		{
+			text: 'Dark',
+			icon: MoonIcon,
+			value: 'dark',
+		},
+		{
+			text: 'System',
+			icon: ComputerDesktopIcon,
+			value: 'system',
+		},
+	];
+
+	return (
+		<>
+			{options.map((option) => (
+				<MenuItem key={option.text}>
+					{({ active }) => (
+						<button
+							onClick={() => setTheme(option.value)}
+							className={`
+                                flex items-center w-full px-4 py-2 text-sm text-gray-700
+                                ${active ? 'bg-gray-100' : ''}
+                                ${theme === option.value ? 'font-semibold' : ''}
+                            `}
+						>
+							<option.icon className="mr-2 h-5 w-5" aria-hidden="true" />
+							{option.text}
+						</button>
+					)}
+				</MenuItem>
+			))}
+		</>
+	);
+}
