3fd3fa3a440d1a5372ea03610ca95b8ba314c453
Dan Edwards <daniel.edwards96@yahoo.com>
2024-08-04 09:16:46 +0100

    Add dark mode styles

--- client/src/app/components/ProfileDropdown.tsx
+++ client/src/app/components/ProfileDropdown.tsx
@@ -46,7 +46,7 @@ export default function ProfileDropdown() {
 				</div>
 				<MenuItems
 					transition
-					className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-gray-100"
+					className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-gray-700 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-gray-100 dark:divide-gray-500"
 				>
 					<div className="py-1">
 						{profileMenu.map((item) => (
@@ -55,7 +55,7 @@ export default function ProfileDropdown() {
 									<NavLink
 										to={item.to}
 										className="
-                                block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                block px-4 py-2 text-sm text-gray-700 dark:hover:text-white dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
 									>
 										{item.name}
 									</NavLink>
